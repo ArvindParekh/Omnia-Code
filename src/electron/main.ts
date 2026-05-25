9217c70d9a627f38328e3d94516dff0eda7d37d7
ArvindParekh <aruparekh@gmail.com>
2026-05-25 19:00:54 -0400

    electron template init

---
+++ src/electron/main.ts
@@ -0,0 +1,29 @@
+import { app, BrowserWindow } from "electron"
+import { ipcMainHandle, isDev } from "./util.js";
+import { getPreloadPath, getUIPath, getIconPath } from "./pathResolver.js";
+import { getStaticData, pollResources } from "./test.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+if (!PORT && isDev()) throw new Error("PORT env variable is not set");
+
+app.on("ready", () => {
+    const mainWindow = new BrowserWindow({
+        // Shouldn't add contextIsolate or nodeIntegration because of security vulnerabilities
+        webPreferences: {
+            preload: getPreloadPath(),
+        }
+        , icon: getIconPath()
+    });
+
+    if (isDev()) mainWindow.loadURL(`http://localhost:${PORT}`)
+    else mainWindow.loadFile(getUIPath());
+
+    pollResources(mainWindow);
+
+    ipcMainHandle("getStaticData", () => {
+        return getStaticData();
+    })
+})
